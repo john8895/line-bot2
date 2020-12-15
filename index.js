c30911e71bbe27ee9818f30c7b079ee89dff51e7
john8895 <paiching01@gmail.com>
2020-12-15 16:44:44 +0800

    update

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ let bot = linebot({
     channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN
 })
 
-bot.on('message', function(event){
+bot.on('message', function (event) {
 
-    event.reply(`你說了 ${event.message.text}`)
+    event.reply(`${event}`)
 })
 
-bot.listen('/', process.env.PORT || 5000, function(){
+bot.listen('/', process.env.PORT || 5000, function () {
     console.log('全國首家LINE線上機器人上線啦！！')
 })
+
+
